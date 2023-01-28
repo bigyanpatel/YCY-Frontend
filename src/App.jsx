2c637005c067a96f194c26f983a78a8539ca2b2d
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2023-01-28 17:04:13 +0530

    Style: Added button hover effect

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 import { HomePage } from './page/PageExport';
 
+
 function App() {
   return (
+    <>
       <HomePage/>
+    </>
   );
 }
 
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 import Twitter from "../../assets/Socials/twitter.png"
 import Instagram from "../../assets/Socials/instagram.png"
 import Linkedin from "../../assets/Socials/linkedin.png"
-import Facebook from "../../assets/Socials/facebook.png"
+import Facebook from "../../assets/Socials/facebook-f.svg"
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import leafs from '../../assets/Footer/v.svg'
 import f_man from '../../assets/Footer/home_two/f_man.png'
@@ -10,6 +12,7 @@ import cloud from '../../assets/Footer/home_two/cloud.png'
 import man from '../../assets/Footer/home_two/man.png'
 import email_icon from '../../assets/Footer/home_two/email-icon.png'
 import email_icon_two from '../../assets/Footer/home_two/email-icon_two.png'
+import { faFacebookF, faInstagram, faLinkedin, faLinkedinIn, faTwitter } from '@fortawesome/free-brands-svg-icons'
 
 export const Footer = () => {
   return (
@@ -24,17 +27,17 @@ export const Footer = () => {
             <div class="flex flex-wrap">
                 <div class="px-4 w-screen lg:w-[33.3%]">
                     <h3 class="font-bold text-lg pb-8">Get In Touch</h3>
-                    <p className='font-bold text-[16px] mb-4 font-roboto'>Want to know about our community ! Then drop a message below. Our Team will contact you.</p>
+                    <p className='font-medium text-[16px] mb-4'>Want to know about our community ! Then drop a message below. Our Team will contact you.</p>
                     <form className='relative shadow-2xl' action="#">
                         <input class="w-[100%] py-[7px] pl-[25px] pr-[12px] rounded-md outline-none" type="email" placeholder="Email" />
                         <button class="absolute right-0 px-[22px] py-[7px] bg-[#EC6B25] rounded-md" type="submit">Send</button>
                     </form>
-                    <ul className='mt-[40px] mb-[16px] flex gap-3'>
-                        <li><a className='w-[46px]' href="" target="_blank"><img className='w-[46px] h-[46px]' src={Facebook}/></a></li>
-                        <li><a className='w-[46px]' href="" target="_blank"><img className='w-[46px] h-[46px]' src={Twitter}/></a></li>
-                        <li><a className='w-[46px]' href="" target="_blank"><img className='w-[46px] h-[46px]' src={Instagram}/></a></li>
-                        <li><a className='w-[46px]' href="" target="_blank"><img className='w-[46px] h-[46px]' src={Linkedin}/></a></li>
-                    </ul>
+                    <div className='mt-[40px] mb-[16px] flex gap-3'>
+                        <a className='hover:shadow-xl duration-300 rounded-[50%]' href="" target="_blank"><div className='w-[46px] h-[46px] flex items-center justify-center rounded-[50%] border-[#EC6B25] border hover:bg-[#17a2b8] hover:border-none duration-300'><FontAwesomeIcon icon={faFacebookF}/></div></a>
+                        <a className='hover:shadow-xl duration-300 rounded-[50%]' href="" target="_blank"><div className='w-[46px] h-[46px] flex items-center justify-center rounded-[50%] border-[#EC6B25] border hover:bg-[#17a2b8] hover:border-none duration-300'><FontAwesomeIcon icon={faLinkedinIn}/></div></a>
+                        <a className='hover:shadow-xl duration-300 rounded-[50%]' href="" target="_blank"><div className='w-[46px] h-[46px] flex items-center justify-center rounded-[50%] border-[#EC6B25] border hover:bg-[#17a2b8] hover:border-none duration-300'><FontAwesomeIcon icon={faInstagram}/></div></a>
+                        <a className='hover:shadow-xl duration-300 rounded-[50%]' href="" target="_blank"><div className='w-[46px] h-[46px] flex items-center justify-center rounded-[50%] border-[#EC6B25] border hover:bg-[#17a2b8] hover:border-none duration-300'><FontAwesomeIcon icon={faTwitter}/></div></a>
+                    </div>
                 </div>
                 <div class="px-4 mt-9 md:mt-0 w-screen sm:w-[50%] lg:w-[33.3%]">
                     <div class="pl-[70px]">
@@ -65,7 +68,7 @@ export const Footer = () => {
             </div>
         </div>
         <div class="h-[66px] bg-[#ec6b25] flex justify-center items-center">
-                <p className='font-bold text-[#6b707f]'>&copy; 2022 All Rights Reserved by <a className='text-[#ffffff]' href="ycy.com">YCYCLASS.COM</a></p>
+            <p className='font-medium text-[#6b707f]'>&copy; 2022 All Rights Reserved by <a className='text-[#ffffff]' href="ycy.com">YCYCLASS.COM</a></p>
         </div>
     </footer>
   )
