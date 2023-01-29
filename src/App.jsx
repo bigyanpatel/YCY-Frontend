ddd99caeacf16d1476627dec083d09ac81857127
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2023-01-29 23:25:46 +0530

    Refactor: Removed unwanted codes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 import './App.css';
 import { HomePage } from './page/PageExport';
 
